bf3403df0918f209e711cd5236f2d5abccb5fc90
Lorenzo Olearo <lorenzo.olearo@olearo.com>
2023-06-19 16:40:12 +0200

    Implement SPI component and its testbench

---
+++ MOALU/src/SPI.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity SPI is
+  generic(
+    DATA_WIDTH : integer := 4
+    );
+
+  port(
+    clk     : in  std_logic;
+    rst     : in  std_logic;
+    bit_in  : in  std_logic;
+    ready   : in  std_logic;
+    bus_out : out std_logic_vector((DATA_WIDTH*2)-1 downto 0) := (others => '0')
+    );
+
+end SPI;
+
+
+architecture SPI_arch of SPI is
+
+  signal shift_register : std_logic_vector((DATA_WIDTH*2)-1 downto 0) := (others => '0');
+
+begin
+
+  shifter : process (clk, rst) is
+  begin
+    if (rst = '1') then
+      shift_register <= (others => '0');
+    elsif clk'event and clk = '1' then
+      shift_register <= bit_in & shift_register(shift_register'high downto 1);
+    end if;
+  end process;
+
+  bus_out <= shift_register;
+
+end SPI_arch;
---
+++ MOALU/src/testbench_SPI.vhd
@@ -0,0 +1,71 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test is
+end entity;
+
+architecture testbench of test is
+
+  constant DATA_WIDTH : integer := 4;
+
+  signal clk     : std_logic                                    := '0';
+  signal rst     : std_logic                                    := '0';
+  signal bit_in  : std_logic                                    := '0';
+  signal bus_out : std_logic_vector((DATA_WIDTH*2)-1 downto 0)  := (others => '0');
+  signal flag    : std_logic                                    := '0';
+  signal bus_in  : std_logic_vector((DATA_WIDTH*2)-1 downto 0)  := "00001111";
+
+
+begin
+
+  SPI : entity work.SPI
+    generic map (
+      DATA_WIDTH => 4
+      )
+    port map (
+      clk     => clk,
+      rst     => rst,
+      bit_in  => bit_in,
+      ready   => '1',
+      bus_out => bus_out
+      );
+
+
+
+  clock_generator : process is
+  begin
+    wait for 10 ns;
+    clk <= not clk;
+  end process;
+
+
+  reset : process is
+  begin
+    rst <= '1';
+    wait for 5 ns;
+    rst <= '0';
+    wait;
+  end process;
+
+
+  -- Times reference:
+  -- (N bits * 20 ns) + 20 ns [flip flop delay] + 10 ns [first bit delay]
+  -- 8 bits * 20 ns = 160 ns
+  -- 160 ns + 20 ns + 10 ns = 190 ns for the entire transmission
+  create_input : process (clk, rst) is
+    variable index : integer := 0;
+  begin
+    if rst = '1' then
+      index := 0;
+    elsif clk'event and clk = '1' then
+      bit_in <= bus_in(index);
+      index  := index + 1;
+      if (index = DATA_WIDTH*2) then
+        index := 0;
+        flag  <= '1';
+      end if;
+    end if;
+  end process;
+
+
+end architecture;
